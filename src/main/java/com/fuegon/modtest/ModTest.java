b3dadb41ad304bdde079e42fb338be252ed59ba1
Fuegon <Fuegon@users.noreply.github.com>
2015-05-28 17:19:20 +0200

    Creating Mod

---
+++ src/main/java/com/fuegon/modtest/ModTest.java
@@ -0,0 +1,28 @@
+package com.fuegon.modtest;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ModTest", name="Mod Test", version="1.8-0.0.1")
+public class ModTest {
+
+    @Mod.Instance("ModTest")
+    public static ModTest instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
